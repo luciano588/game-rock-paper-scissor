b78ef9131fa3bc6267ebb07064ea9ebb93aa8b02
luciano588 <luciano588.alm@gmail.com>
2020-12-05 15:41:04 +0000

    first commit

---
+++ src/js/component/footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+//create your first component
+export function Footer() {
+	return <div className="text-center mt-5">footer</div>;
+}
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,51 @@
 import React from "react";
+import { Navbar } from "./navbar";
+import { Jumbotron } from "./jumbtron";
+import { Card } from "./card";
+import { Footer } from "./footer";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+const data = [
+	{
+		cardTitle: "card01",
+		cardDescription: " description"
+	},
+	{
+		cardTitle: "card02",
+		cardDescription: " description"
+	},
+	{
+		cardTitle: "card03",
+		cardDescription: " description"
+	},
+	{
+		cardTitle: "card04",
+		cardDescription: " description"
+	},
+	{
+		cardTitle: "card05",
+		cardDescription: " description"
+	}
+];
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<Navbar />
+			<Jumbotron />
+			<div className="container">
+				<div className="row">
+					{data.map((item, index) => {
+						return (
+							<div key={index} className="col-sm">
+								<Card title={item.cardTitle} />{" "}
+							</div>
+						);
+					})}
+				</div>{" "}
+			</div>
+			<Footer />
 		</div>
 	);
 }
